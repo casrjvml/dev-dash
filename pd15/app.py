3abd263a8526855ef2be1dd225a99844023029f2
casrjvml <carlos2206@uol.com.br>
2026-08-22 17:46:57 +0000

    Cria Dash interativa

--- pd15/app.py
+++ pd15/app.py
@@ -1,7 +1,11 @@
 # Bibliotecas
-from shiny import ui, render, App
+from shiny import ui, render, App, reactive
 from bcb import currency
 import pandas as pd
+import shinyswatch as ssw
+import plotnine as p9
+
+
 
 # Interface do Usuário
 app_ui = ui.page_navbar(
@@ -24,19 +28,50 @@ app_ui = ui.page_navbar(
                     language="pt-BR",
                     separator="-"
                 ),
+                ui.input_radio_buttons(
+                    id = "tipo",
+                    label = "Tipo de taxa:",
+                    choices = {"bid": "Compra", "ask": "Venda"}
+                ),
                 width = 275 
 
             ),
-            ui.card("output")
+            ui.card(
+                ui.card_header("Taxa de câmbio"),
+                ui.output_plot("grafico"),
+                ui.card_footer("Dados: BCB | Elaboração: Carlos Castro"))
         )
     ),
-    title="Câmbio App"
+    title="Câmbio App",
+    theme = ssw.theme.cyborg
 )
 
 
 #Servidor
 def server(input, output, session):
-    ...
+
+    @reactive.calc
+    def coleta_dados():
+        dados = currency.get(
+            symbols=input.moeda(),
+            start= input.periodo()[0],
+            end= input.periodo()[1],
+            side = "both")[input.moeda()].reset_index()
+        return dados
+
+    @render.plot
+    def grafico():
+        return(
+            p9.ggplot(coleta_dados())+
+            p9.aes(x="Date", y= input.tipo())+
+            p9.geom_line() +
+            p9.labs(
+                y = input.moeda().upper() + " / BRL",
+                x=""
+            )
+        )
+        
+
 
 
 
